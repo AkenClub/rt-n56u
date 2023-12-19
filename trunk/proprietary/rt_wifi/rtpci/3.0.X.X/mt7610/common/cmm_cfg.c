70bd3f1ef9cdff23b39b3b2445f8d86fdbf16c31
Aken <52781316+AkenClub@users.noreply.github.com>
2023-12-19 16:04:30 +0800

    Update cmm_cfg.c

--- trunk/proprietary/rt_wifi/rtpci/3.0.X.X/mt7610/common/cmm_cfg.c
+++ trunk/proprietary/rt_wifi/rtpci/3.0.X.X/mt7610/common/cmm_cfg.c
@@ -586,7 +586,7 @@ INT RT_CfgSetWPAPSKKey(
 
 	if ((keyStringLen < 8) || (keyStringLen > 64))
 	{
-		DBGPRINT(RT_DEBUG_TRACE, ("WPAPSK Key length(%d) error, required 8 ~ 64 characters!(keyStr=%s)\n", 
+		DBGPRINT(RT_DEBUG_TRACE, ("3.0.xx.7610.cfg.WPAPSK Key length(%d) error, required 8 ~ 64 characters!(keyStr=%s)\n", 
 									keyStringLen, keyString));
 		return FALSE;
 	}
